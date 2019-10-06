136c8b2c618d1cb74122a775f8c01739e57204f2
Paul Elder <paul.elder@ideasonboard.com>
2019-10-06 17:29:03 -0400

    Simulator: fix style

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,10 +1,3 @@
-/*
-	Simulator (skeleton)
-
-	Author: Maher Khan
-
- */
-
 import java.util.HashMap;
 import java.lang.IllegalArgumentException;
 
@@ -12,35 +5,23 @@ public class Simulator
 {
 	private int cycle;
 
-
-	/*
-		constructor
-	 */
 	public Simulator()
 	{
 		this.cycle = 0;
 	}
 
-
-	/*
-		This function should simulate a single cycle of the architecture
-	 */
 	private void run_cycle()
 	{
 		this.cycle = this.cycle + 1;
 	}
 
-	/*
-		Simulates Part 1
-	 */
+	/* Simulates Part 1 */
 	public void run(InstructionCache instruction_cache, Memory memory)
 	{
 		run_cycle();
 	}
 
-	/*
-	Simulates Part 2
-	 */
+	/* Simulates Part 2 */
 	public void run_smt(InstructionCache instruction_cache_1, InstructionCache instruction_cache_2, Memory memory)
 	{
 		run_cycle();
@@ -48,46 +29,24 @@ public class Simulator
 
 	public static void main(String[] args) 
 	{
-		if(args.length == 0)
-		{
+		if (args.length == 0) {
 			throw new IllegalArgumentException("No test file passed to the simulator!");
-		}
-		else if(args.length==1)
-		{
-			// Load input file path
+		} else if (args.length == 1) {
 			String filepath = args[0];
-
-			// Create new instruction cache object
 			InstructionCache instruction_cache = new InstructionCache();
-
-			// Create new memory object
 			Memory memory = new Memory();
 
-			// Parse the input file and fill the instruction cache and memory
 			Parser.parseFile(filepath, instruction_cache, memory);
-
-			// Create new instance of the Simulator
 			Simulator simulator = new Simulator();
-
-			// Run the simulator
 			simulator.run(instruction_cache, memory);
 
-			// Print Instruction Cache
 			System.out.println(instruction_cache.toString());
-
-			// Print Registers
-			
-			// Print Data Memory
+			// TODO: Print Registers
 			System.out.println(memory.toString());
-			
+			// TODO: Print cycle count and other information
 
-			// Print cycle count and other information
-
-		}
-
-		else if(args.length==2)
-		{
-			//Part2: SMT
+		} else if (args.length == 2) {
+			// Part2: SMT
 			
 			// Load input file paths
 			String filepath_1 = args[0];
