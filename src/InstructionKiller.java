b3c6895d36e8406b12ec9a5babc523837a089fdf
NathanAckerman <nga5@pitt.edu>
2019-10-17 23:38:22 -0400

    added InstructionKiller file

---
+++ src/InstructionKiller.java
@@ -0,0 +1,27 @@
+public class InstructionKiller {
+	Simulator sim;
+
+	InstructionKiller(Simulator the_sim)
+	{
+		sim = the_sim;
+	}
+
+	public void killInstructionAnywhere(Instruction instr)
+	{
+		//TODO what is going on with the buffers for wb?
+		
+		//find and kill the instruction if in units
+		boolean killed_in_units = units.killInstr(instr);
+		
+		//find and kill the instruction if in the issuer
+		if (!killed_in_units) {
+			sim.issuer.killInstr(instr);
+		}
+
+		//need to unmap the register it reserved because it was going to write into it
+		if (instr.dest_reg_str != null) {
+			sim.rename_table.removeRename(instr.dest_reg_str);
+		}
+	}
+
+}
